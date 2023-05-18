c8190db0a9bf427d415f85170200227cd481bf35
bowfun <bowfun@bownet.xyz>
2023-05-18 07:43:31 -0600

    Add description and name
    Fun! The site is nearly ready!

--- pages/view.js
+++ pages/view.js
@@ -8,7 +8,6 @@ const inter = Inter({ subsets: ['latin'] });
 import id_0 from '../public/animations/id_0.gif';
 import id_1 from '../public/animations/id_1.gif';
 import id_2 from '../public/animations/id_2.gif';
-import id_3 from '../public/animations/id_3.gif';
 
 export default function ViewSite() {
     const router = useRouter();
@@ -46,15 +45,39 @@ export default function ViewSite() {
             return id_1;
         } else if (animationId === 2) {
             return id_2;
-        } else if (animationId === 3) {
-            return id_3;
         }
         return null;
     };
+    const getAnimationName = () => {
+        const name_0 = "Spinning Color Wheel"
+        const name_1 = "Spinning Water Bottle"
+        const name_2 = "Skipping Rock"
+        if (animationId === 0) {
+            return name_0;
+        } else if (animationId === 1) {
+            return name_1;
+        } else if (animationId === 2) {
+            return name_2;
+        }
+        return null;
+    }
+    const getAnimationDesc = () => {
+        const desc_0 = "This is a spinning wheel of vibrant colors. It could symbolize a wheel or the top of a spinning umbrella."
+        const desc_1 = "This is a spinning water bottle. Stay hydrated during the summer. It is symbolic for throwing a water bottle in the air and grabbing it."
+        const desc_2 = "This is a skipping rock. The rock skips across the lake. It could be related to moving on."
+        if (animationId === 0) {
+            return desc_0;
+        } else if (animationId === 1) {
+            return desc_1;
+        } else if (animationId === 2) {
+            return desc_2;
+        }
+        return null;
+    }
 
     return (
         <main
-            className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+            className={`flex min-h-screen flex-col items-center justify-between p-64 ${inter.className}`}
         >
             {animationId !== null && (
                 <Image
@@ -63,6 +86,12 @@ export default function ViewSite() {
                     width={300}
                     height={125}
                 />
+            )}
+            {animationId !== null && (
+                <h1 style={{ fontSize: '24px' }}>{getAnimationName()}</h1>
+            )}
+            {animationId !== null && (
+                <p style={{ fontSize: '18px' }}>{getAnimationDesc()}</p>
             )}
         </main>
     );
